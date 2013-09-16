1708af8ac79beb11e579c1de279f1c99e25d15a1
darrenli <darren@thanx.com>
2013-09-15 21:18:36 -0700

    Update sleepimage script.

--- install/install-config.sh
+++ install/install-config.sh
@@ -1,5 +1,5 @@
-echo "Hibernate mode set to desktop"
+echo "Ensure no sleepimage files are generated"
 sudo pmset -a hibernatemode 0
-
-echo "Clean up sleep image"
 sudo rm /private/var/vm/sleepimage
+sudo touch /private/var/vm/sleepimage
+sudo chflags uchg /private/var/vm/sleepimage
