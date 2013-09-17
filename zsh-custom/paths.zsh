2835c56f3298f322a79c31e4cc2533cb903957d9
darrenli <darren@thanx.com>
2013-09-16 18:20:00 -0700

    Load RVM via zshenv instead of zsh-custom paths.

--- zsh-custom/paths.zsh
+++ zsh-custom/paths.zsh
@@ -6,5 +6,3 @@ export PATH=/usr/local/bin:$PATH
 export PATH=/usr/local/mysql/bin:$PATH
 # Include Android tools in PATH
 export PATH=~/Developer/library/android-sdk-macosx/tools:$PATH
-# Load RVM into a shell session *as a function*
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
