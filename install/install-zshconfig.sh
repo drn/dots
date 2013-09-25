4e7620a5e264433f86d9f4e6015ba0b203776177
darrenli <darren@thanx.com>
2013-09-24 23:39:53 -0700

    Adjust clone usage for other cloned repos.

--- install/install-zshconfig.sh
+++ install/install-zshconfig.sh
@@ -9,7 +9,7 @@ source "$dotfiles/install/install.cfg"
 
 # install oh-my-zsh
 sudo rm -rf $ohmyzsh
-clone git://github.com/robbyrussell/oh-my-zsh.git $ohmyzsh
+clone robbyrussell/oh-my-zsh $ohmyzsh
 
 # install custom oh-my-zsh config files
 for location in $dotfiles/zsh/*; do
@@ -19,4 +19,4 @@ done
 
 # install zsh-syntax-highlighting
 mkdir -p "$ohmyzsh/custom/plugins"
-clone git://github.com/zsh-users/zsh-syntax-highlighting.git "$ohmyzsh/custom/plugins/zsh-syntax-highlighting"
+clone zsh-users/zsh-syntax-highlighting "$ohmyzsh/custom/plugins/zsh-syntax-highlighting"
