6735f882291ccd24ab3eddaeaeff100c657a3610
darrenli <darren@thanx.com>
2013-09-27 01:12:29 -0700

    Ensure sudo access in base install script.

--- install/install.sh
+++ install/install.sh
@@ -6,6 +6,9 @@ dotfiles="$dev/dotfiles"
 
 echo "Installing SanguineRane configuration for $(whoami)"
 
+# ensure sudo access
+sudo -p "Password for sudo: " echo "We're good to go!"
+
 # change directory to home, in order to avoid directory conflicts
 cd ~
 
