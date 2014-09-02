0487dd30938e9606d00f83d308586ad36671aca5
darrenli <darren@thanx.com>
2014-09-02 17:38:21 -0500

    Add neovim clipboard support via python client.

---
+++ vim/pythonx/nvim_clipboard.py
@@ -0,0 +1,11 @@
+import xerox
+
+class NvimClipboard(object):
+  def __init__(self, vim):
+    self.provides = ['clipboard_get', 'clipboard_set']
+
+  def clipboard_get(self):
+    return xerox.paste().split('\n')
+
+  def clipboard_set(self, lines):
+    xerox.copy('\n'.join(lines))
