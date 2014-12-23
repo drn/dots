e390d71cf424e97b400a612ab5729446c8f174d2
Darren Cheng <darren@thanx.com>
2014-12-23 01:05:59 -0800

    Add menubar item to caffeine hammerspoon module.

--- hammerspoon/caffeine.lua
+++ hammerspoon/caffeine.lua
@@ -1,12 +1,29 @@
 local caffeine = {}
 
 local sleepType = 'system'
+local menuItem = nil
+
 local function alert(message) hs.alert.show(message, 1) end
 local function isOn() return hs.caffeinate.get(sleepType) end
+local function createMenuItem()
+  if menuItem ~= nil then return end
+  menuItem = hs.menubar.new()
+  menuItem:setIcon(os.getenv('HOME')..'/.hammerspoon/caffeine.png')
+  menuItem:setClickCallback(function() caffeine.toggle() end)
+end
+local function deleteMenuItem()
+  if menuItem == nil then return end
+  menuItem:delete()
+  menuItem = nil
+end
+local function toggleMenu()
+  if isOn() then createMenuItem() else deleteMenuItem() end
+end
 
 function caffeine.toggle()
   hs.caffeinate.set(sleepType, not isOn(), true)
   alert(isOn() and 'Caffeinating!' or 'Getting sleepy...')
+  toggleMenu()
 end
 
 function caffeine.display()
