427ae8380f3a0ae659bef75a0fb2a6f695771a6b
Darren Cheng <darren@thanx.com>
2014-12-28 02:09:55 -0500

    Add initial iTerm screen change logic.

--- hammerspoon/screen.lua
+++ hammerspoon/screen.lua
@@ -9,10 +9,18 @@ local function handleCountChange(count)
   if count == 1 then
     -- TODO
   elseif count == 2 then
+    app = hs.application.applicationsForBundleID('com.googlecode.iterm2')[1]
+    if app ~= nil then
+      for _,win in pairs(app:allWindows()) do
+        if win:title():match('vertical') then
+          -- TODO
+          hs.alert.show('Move iTerm vertical to next screen')
+        end
+      end
+    end
     -- TODO
-      -- if open, move Mailplane to next screen, bottom
-      -- if open, move iTerm vertical window to next screen, full screen
-      -- if open, move Messages to next screen, bottom
+    -- if open, move Mailplane to next screen, bottom
+    -- if open, move Messages to next screen, bottom
   end
 end
 
