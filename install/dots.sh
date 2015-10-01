e8ac542afd0d8ecb933d3e9282d59362c57a0fd3
Darren Cheng <darren@thanx.com>
2015-09-30 17:22:07 -0700

    Remove extra stdout log in install/dots.

--- install/dots.sh
+++ install/dots.sh
@@ -4,6 +4,5 @@ source "$HOME/.dots/install/core.cfg"
 # ~ files
 for location in $HOME/.dots/home/*; do
   file="${location##*/}"
-  echo "$location to $HOME/.$file"
   link "$location" "$HOME/.$file"
 done
