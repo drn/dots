318b5128ad7a9e757775f7db48ed5740e4200740
Darren Cheng <darren@thanx.com>
2015-12-14 13:20:10 -0800

    Auto-rename tmux window after z jump.

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -48,6 +48,18 @@ HISTSIZE=10000
 
 # Initialize Z
 . /usr/local/etc/profile.d/z.sh
+# Auto-rename tmux window
+function _z_wrapper() {
+  _z $1
+  if [ -n "$1" ]; then
+    if [[ $PWD == *"/thanx-"* ]]; then
+      if [ "$(tmux display-message -p '#{window_panes}')" = "1" ]; then
+        tmux rename-window $(echo $PWD | sed 's/.*\/thanx-//')
+      fi
+    fi
+  fi
+}
+alias ${_Z_CMD:-z}='_z_wrapper 2>&1'
 
 # Autoload tmux
 if [ "$TMUX" = "" ]; then smux master; fi
