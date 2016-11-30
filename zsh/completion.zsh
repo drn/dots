5cd5679f01d7d0452384e03572e83c7f7f16a1bc
Darren Cheng <darren@thanx.com>
2016-11-29 23:28:15 -0800

    Adjust zsh select-word-style.
    https://github.com/zsh-users/zsh-syntax-highlighting/issues/67

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -14,3 +14,7 @@ zstyle ':completion:*' menu yes select
 zstyle ':completion:*' list-colors "${(@s.:.)LS_COLORS}"
 # case insensitive, hyphen-insensitive, partial-word and substring completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+
+# adjust select-word-style
+autoload -U select-word-style
+select-word-style bash
