794dc896cfacdefc8294abd3672601564f4feec5
Darren Cheng <darren@thanx.com>
2017-01-29 19:39:56 -0800

    Remove unused playlist hs bindings.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -75,12 +75,6 @@ hs.hotkey.bind({ 'ctrl', 'cmd' }, 'right', function() itunes.forward() end)
 hs.hotkey.bind({ 'ctrl', 'cmd' }, 'left', function() itunes.backward() end)
 hs.hotkey.bind({ 'cmd', 'alt' }, 'up', function() itunes.increaseVolume() end)
 hs.hotkey.bind({ 'cmd', 'alt' }, 'down', function() itunes.decreaseVolume() end)
-hs.hotkey.bind({ 'ctrl', 'shift' }, 'space', function()
-  itunes.addToPlaylist('Dunno')
-end)
-hs.hotkey.bind({ 'ctrl', 'shift' }, ',', function()
-  itunes.addToPlaylist('Erg')
-end)
 
 -- Chrome
 
