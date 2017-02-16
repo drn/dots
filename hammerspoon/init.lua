02df8f9bc772b650ebf80cd2bcc1c2e14bd866db
Darren Cheng <darren@thanx.com>
2017-02-15 19:19:22 -0800

    Adjust chrome refocus binding keys.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -79,7 +79,7 @@ hs.hotkey.bind({ 'ctrl', 'cmd' }, 'space', function() hs.alert('space') end)
 
 -- Chrome
 
-hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'm', function()
+hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'l', function()
   chrome.refocus()
 end)
 hs.hotkey.bind({'ctrl', 'alt', 'cmd'}, 'n', function()
