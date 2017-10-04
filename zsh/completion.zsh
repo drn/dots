300fbbac64a3a3f00630c5bf9164257f7f5df174
Darren Cheng <darren@thanx.com>
2017-10-04 00:22:30 -0700

    Delegate _git_f completion to _git_fetch.

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,10 +1,9 @@
 autoload -U compaudit compinit
 
-# delegate _git_rb completion to _git_rebase
-_git_rb () { _git_rebase }
-
-# delegate _git_cp completion to _git_cherry_pick
-_git_cp () { _git_cherry_pick }
+# completion delegation
+_git_rb() { _git_rebase }
+_git_cp() { _git_cherry_pick }
+_git_f()  { _git_fetch }
 
 # git completion
 source /usr/local/etc/bash_completion.d/git-completion.bash 2>/dev/null
