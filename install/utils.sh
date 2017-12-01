08ac26b5b9052f186c6a740a5ec12897e5e7305d
Darren Cheng <darren@thanx.com>
2017-11-30 23:51:49 -0800

    Install neovim python dependencies.

--- install/utils.sh
+++ install/utils.sh
@@ -19,5 +19,30 @@ gem install bundler
 bundle install --gemfile=$HOME/.dots/Gemfile
 rm -f $HOME/.dots/Gemfile.lock
 
+# configure neovim python dependencies
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+# install python versions
+pyenv install 2.7.11 -s
+pyenv install 3.4.4 -s
+# create virtualenvs
+pyenv virtualenv 2.7.11 neovim2 || true
+pyenv virtualenv 3.4.4 neovim3 || true
+# neovim python 2
+pyenv activate neovim2
+pip install --upgrade pip
+pip install neovim
+pyenv which python
+# neovim python 3
+pyenv activate neovim3
+pip install --upgrade pip
+pip install neovim
+pyenv which python
+# install flake8 liner
+pip install flake8
+ln -s `pyenv which flake8` ~/bin/flake8
+# deactivate pyenv
+pyenv deactivate
+
 # install wakatime
-pip install wakatime
+pip2 install wakatime
