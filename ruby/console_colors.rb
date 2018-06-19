e767ea8180a81f72d1540418e53eaf007ce1bd1d
Darren Cheng <darren@thanx.com>
2018-06-18 18:07:42 -0700

    Share ConsolePrompt with ~/.pryrc and ~/.irbrc.

---
+++ ruby/console_colors.rb
@@ -0,0 +1,20 @@
+CONSOLE_COLORS = {
+  reset:         "\e[0m",
+  bold:          "\e[1m",
+  black:         "\e[30m",
+  red:           "\e[31m",
+  green:         "\e[32m",
+  yellow:        "\e[33m",
+  blue:          "\e[34m",
+  magenta:       "\e[35m",
+  cyan:          "\e[36m",
+  light_gray:    "\e[37m",
+  dark_gray:     "\e[90m",
+  light_red:     "\e[91m",
+  light_green:   "\e[92m",
+  light_yellow:  "\e[93m",
+  light_blue:    "\e[94m",
+  light_magenta: "\e[95m",
+  light_cyan:    "\e[96m",
+  white:         "\e[97m"
+}.freeze
---
+++ ruby/console_prompt.rb
@@ -0,0 +1,99 @@
+require "#{ENV['HOME']}/.dots/ruby/console_colors"
+
+class ConsolePrompt
+  def initialize(prefix, object=nil, level=0)
+    @prefix = prefix
+    @object = object
+    @level = level
+  end
+
+  def normal
+    prefix + info + context + level + suffix(:cyan)
+  end
+
+  def continue
+    prefix + info + context + level + suffix(:yellow)
+  end
+
+  def return
+    "#{colors[:light_yellow]}=>#{colors[:reset]} %s\n"
+  end
+
+private
+
+  def prefix
+    prefix = [
+      separator,
+      separator(:blue),
+      separator,
+      ' '
+    ]
+
+    unless @prefix.nil?
+      prefix = [
+        colors[:bold],
+        colors[:dark_gray],
+        @prefix,
+        colors[:reset],
+        ' '
+      ] + prefix
+    end
+
+    prefix.join
+  end
+
+  def info
+    info = [
+      colors[:reset],
+      colors[:bold],
+      colors[:blue]
+    ]
+    if ENV.key?('PROMPT_NAME')
+      info << ENV['PROMPT_NAME']
+    elsif defined?(Rails) && Rails.respond_to?(:application)
+      info << Rails.application.class.name.gsub('::Application','')
+    else
+      info << RUBY_VERSION.to_s
+    end
+    info << colors[:reset]
+    info.join
+  end
+
+  def context
+    return '' unless defined?(Pry)
+    context = Pry.view_clip(@object)
+    return '' if context == 'main'
+    context = context.to_s.gsub('#<','').gsub('>','')
+    context = '*::' + context.gsub(/.*::/,'') if context.include?('::')
+    [
+      ' ',
+      colors[:light_red],
+      context,
+      colors[:reset]
+    ].join
+  end
+
+  def level
+    return '' if @level.zero?
+    [
+      ' ',
+      colors[:reset],
+      colors[:bold],
+      colors[:magenta],
+      @level.to_s,
+      colors[:reset]
+    ].join
+  end
+
+  def suffix(color)
+    ' ' + separator(color) + ' ' + colors[:reset]
+  end
+
+  def separator(color=:magenta)
+    "#{colors[:bold]}#{colors[color]}❯#{colors[:reset]}"
+  end
+
+  def colors
+    CONSOLE_COLORS
+  end
+end
