5eb3261622fdf5a3abaaa4c647f96dcfabbb6aba
Darren Cheng <darren@thanx.com>
2018-07-15 16:14:01 -0700

    Add logging to install/zsh.

--- commands/install/zsh.go
+++ commands/install/zsh.go
@@ -11,16 +11,19 @@ func Zsh() {
 
   // delete /etc/zprofile - added by os x 10.11
   // path_helper conflicts - http://www.zsh.org/mla/users/2015/msg00727.html
+  color.Blue("Ensuring /etc/zprofile is removed")
   if util.IsFileExists("/etc/zprofile") {
     util.Run("sudo rm -f /etc/zprofile")
   }
 
   // ensure antibody is installed
+  color.Blue("Ensuring antibody is installed")
   if util.IsCommand("brew") && !util.IsCommand("antibody") {
     color.Blue("Installing antibody...")
     util.Run("brew install getantibody/tap/antibody 2>/dev/null")
   }
 
   // run antibody bundle
+  color.Blue("Bundling antibody dependencies")
   util.Run("antibody bundle < \"$DOTS/zsh/bundles\" > ~/.bundles")
 }
