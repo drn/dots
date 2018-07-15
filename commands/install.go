356b74a7ae1ea2036d5ddc16b1d6de1bf08b3566
Darren Cheng <darren@thanx.com>
2018-07-15 15:52:42 -0700

    Add top-level `install` command.

--- commands/install.go
+++ commands/install.go
@@ -8,13 +8,39 @@ import (
   "github.com/spf13/cobra"
   "github.com/fatih/color"
   "github.com/drn/dots/util"
+  "github.com/manifoldco/promptui"
 )
 
 var cmdInstall = &cobra.Command{
   Use: "install",
   Short: "Installs configuration",
   Run: func(cmd *cobra.Command, args []string) {
-    cmd.Help()
+    prompt := promptui.Select{
+      Label: "Select component to install",
+      Items: []string{
+        "bin",
+        "git",
+        "home",
+        "zsh",
+        "hammerspoon",
+      },
+    }
+    _, result, err := prompt.Run()
+
+    if err != nil { os.Exit(1) }
+
+    switch result {
+    case "bin":
+      installBin()
+    case "git":
+      installGit()
+    case "home":
+      installHome()
+    case "zsh":
+      installZsh()
+    case "hammerspoon":
+      installHammerspoon()
+    }
   },
 }
 
@@ -30,50 +56,67 @@ var cmdInstallBin = &cobra.Command{
   Use: "bin",
   Short: "Installs ~/bin/* commands",
   Run: func(cmd *cobra.Command, args []string) {
-    link("lib/bin", "bin")
+    installBin()
   },
+}
+
+func installBin() {
+  link("lib/bin", "bin")
 }
 
 var cmdInstallGit = &cobra.Command{
   Use: "git",
   Short: "Installs git extensions",
   Run: func(cmd *cobra.Command, args []string) {
-    link("lib/git/functions", ".git-extensions")
+    installGit()
   },
+}
+
+func installGit() {
+  link("lib/git/functions", ".git-extensions")
 }
 
 var cmdInstallHome = &cobra.Command{
   Use: "home",
   Short: "Installs ~/.* config files",
   Run: func(cmd *cobra.Command, args []string) {
-    color.Blue("Installing ~/.* files...")
-
-    files, _ := ioutil.ReadDir(fmt.Sprintf("%s/lib/home", dotsPath()))
-    for _, file := range files {
-      link(
-        fmt.Sprintf("lib/home/%s", file.Name()),
-        fmt.Sprintf(".%s", file.Name()),
-      )
-    }
+    installHome()
   },
+}
+
+func installHome() {
+  color.Blue("Installing ~/.* files...")
+
+  files, _ := ioutil.ReadDir(fmt.Sprintf("%s/lib/home", dotsPath()))
+  for _, file := range files {
+    link(
+      fmt.Sprintf("lib/home/%s", file.Name()),
+      fmt.Sprintf(".%s", file.Name()),
+    )
+  }
 }
 
 var cmdInstallZsh = &cobra.Command{
   Use: "zsh",
   Short: "Installs zsh config files",
   Run: func(cmd *cobra.Command, args []string) {
-    // delete /etc/zprofile - added by os x 10.11
-    // path_helper conflicts - http://www.zsh.org/mla/users/2015/msg00727.html
-    util.Run("sudo rm -f /etc/zprofile")
-
-    // ensure antibody is installed
-    if util.IsCommand("brew") {
-      util.Run("brew install getantibody/tap/antibody")
-    }
-
-    // run antibody bundle
-    util.Run("antibody bundle < \"$DOTS/zsh/bundles\" > ~/.bundles")
+    installZsh()
   },
+}
+
+func installZsh() {
+  // delete /etc/zprofile - added by os x 10.11
+  // path_helper conflicts - http://www.zsh.org/mla/users/2015/msg00727.html
+  util.Run("sudo rm -f /etc/zprofile")
+
+  // ensure antibody is installed
+  if util.IsCommand("brew") && !util.IsCommand("antibody") {
+    color.Blue("Installing antibody...")
+    util.Run("brew install getantibody/tap/antibody 2>/dev/null")
+  }
+
+  // run antibody bundle
+  util.Run("antibody bundle < \"$DOTS/zsh/bundles\" > ~/.bundles")
 }
 
 var cmdInstallHammerspoon = &cobra.Command{
@@ -81,13 +124,17 @@ var cmdInstallHammerspoon = &cobra.Command{
   Aliases: []string{ "hs" },
   Short: "Installs hammerspoon configuration files",
   Run: func(cmd *cobra.Command, args []string) {
-    link("lib/hammerspoon", ".hammerspoon")
-    util.Osascript(
-      "tell application \"%s\" to execute lua code \"%s\"",
-      "Hammerspoon",
-      "hs.reload()",
-    )
+    installHammerspoon()
   },
+}
+
+func installHammerspoon() {
+  link("lib/hammerspoon", ".hammerspoon")
+  util.Osascript(
+    "tell application \"%s\" to execute lua code \"%s\"",
+    "Hammerspoon",
+    "hs.reload()",
+  )
 }
 
 func link(from string, to string) {
