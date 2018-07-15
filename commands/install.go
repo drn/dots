23fbd34a0efc08b47b125ba7519f97203785117c
Darren Cheng <darren@thanx.com>
2018-07-15 13:26:08 -0700

    Add `install hammerspoon` command.

--- commands/install.go
+++ commands/install.go
@@ -1,6 +1,9 @@
 package commands
 
 import (
+  "os"
+  "fmt"
+  "os/user"
   "github.com/spf13/cobra"
   "github.com/fatih/color"
 )
@@ -15,6 +18,7 @@ var cmdInstall = &cobra.Command{
 
 func init() {
   cmdInstall.AddCommand(cmdInstallDots)
+  cmdInstall.AddCommand(cmdInstallHammerspoon)
 }
 
 var cmdInstallDots = &cobra.Command{
@@ -23,4 +27,40 @@ var cmdInstallDots = &cobra.Command{
   Run: func(cmd *cobra.Command, args []string) {
     color.Green("Installing ~/.* files...")
   },
+}
+
+var cmdInstallHammerspoon = &cobra.Command{
+  Use: "hammerspoon",
+  Aliases: []string{ "hs" },
+  Short: "Installs hammerspoon configuration files",
+  Run: func(cmd *cobra.Command, args []string) {
+    link("lib/hammerspoon", ".hammerspoon")
+  },
+}
+
+func link(from string, to string) {
+  color.Blue("Linking '$DOTS/%s' to '~/%s'", from, to)
+
+  from = fmt.Sprintf("%s/%s", dotsPath(), from)
+  to = fmt.Sprintf("%s/%s", homePath(), to)
+
+  // overwrite existing symlinks
+  if _, err := os.Lstat(to); err == nil {
+    os.Remove(to)
+  }
+
+  // create symlink
+  err := os.Symlink(from, to)
+
+  // log errors
+  if err != nil { color.Red(err.Error()) }
+}
+
+func dotsPath() string {
+  return fmt.Sprintf("%s/go/src/github.com/drn/dots", homePath())
+}
+
+func homePath() string {
+  user, _ := user.Current()
+  return user.HomeDir
 }
