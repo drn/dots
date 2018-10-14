e99305f3cd1b41a4e43871c0cc38d14d3917a43f
Darren Cheng <darren@thanx.com>
2018-10-14 15:11:44 -0700

    Rewrite battery script in golang.

---
+++ bin/battery/root.go
@@ -0,0 +1,19 @@
+// Prints the current battery remaining percentage
+
+package main
+
+import (
+  "fmt"
+  "strings"
+  "github.com/drn/dots/run"
+)
+
+func main() {
+  info := run.Capture("pmset -g ps")
+  info = strings.Split(info, "\n")[1]
+  // extract percent from pmset metadata
+  percent := strings.Fields(info)[2]
+  // remove trailing ;
+  percent = percent[:len(percent)-1]
+  fmt.Println(percent)
+}
