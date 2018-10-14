41c16bcafb1ef58c1431c90630b65e398badf39f
Darren Cheng <darren@thanx.com>
2018-10-14 13:57:51 -0700

    Rewrite ssid command in go.

---
+++ bin/ssid/root.go
@@ -0,0 +1,33 @@
+// If available, prints out WiFi SSID currently connected to.
+
+package main
+
+import (
+  "fmt"
+  "os"
+  "strings"
+  "github.com/drn/dots/run"
+)
+
+func main() {
+  info := run.Capture("airport -I")
+
+  if strings.Contains(info, "AirPort: Off") {
+    fmt.Println("[ disabled ]")
+    os.Exit(0)
+  }
+
+  lines := strings.Split(info, "\n")
+
+  for _, line := range lines {
+    if strings.Contains(line, " SSID: ") {
+
+      ssid := strings.Replace(line, " SSID: ", "", 1)
+      ssid = strings.TrimSpace(ssid)
+
+      fmt.Println(ssid)
+
+      os.Exit(0)
+    }
+  }
+}
