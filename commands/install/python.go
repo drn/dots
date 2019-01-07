d6c03235b4bcf6d7b061bb666ab6a8765cb2a1da
Darren Cheng <darren@thanx.com>
2019-01-06 17:23:39 -0800

    Ensure python install handles virtualenvs in subshells.

--- commands/install/python.go
+++ commands/install/python.go
@@ -14,10 +14,6 @@ import (
 func (i Install) Python() {
   log.Action("Installing Python")
 
-  log.Info("Configuring neovim python dependencies")
-  run.Verbose("eval \"$(pyenv init -)\"")
-  run.Verbose("eval \"$(pyenv virtualenv-init -)\"")
-
   log.Info("Installing python versions")
   run.Verbose("pyenv install 2.7.11 -s")
   run.Verbose("pyenv install 3.4.4 -s")
@@ -26,26 +22,28 @@ func (i Install) Python() {
   run.Verbose("pyenv virtualenv 3.4.4 neovim3 || true")
 
   log.Info("Installing python2 neovim dependencies")
-  run.Verbose("pyenv activate neovim2")
-  run.Verbose("pip install --upgrade pip")
-  run.Verbose("pip install --upgrade neovim")
-  run.Verbose("pyenv which python")
-
-  log.Info("Installing python3 neovim dependencies")
-  run.Verbose("pyenv activate neovim3")
-  run.Verbose("pip install --upgrade pip")
-  run.Verbose("pip install --upgrade neovim")
-  run.Verbose("pyenv which python")
-
-  log.Info("Installing flake8 python linter")
-  run.Verbose("pip install --upgrade flake8")
-  link.Soft(
-    run.Capture("pyenv which flake8"),
-    path.FromHome("bin/flake8"),
+  neovim2 := "eval \"$(pyenv init -)\" && pyenv shell neovim2"
+  run.Verbose(
+    "%s && %s && %s",
+    "pyenv exec pip install --upgrade pip pynvim",
+    "pyenv which python",
+    neovim2,
   )
 
-  log.Info("Deactivating pyenv")
-  run.Verbose("pyenv deactivate")
+  log.Info("Installing python3 neovim dependencies")
+  neovim3 := "eval \"$(pyenv init -)\" && pyenv shell neovim3"
+  run.Verbose(
+    "%s && %s && %s",
+    "pyenv exec pip install --upgrade pip pynvim flake8",
+    "pyenv which python",
+    neovim3,
+  )
+
+  log.Info("Linking neovim3 flake8 python linter")
+  link.Soft(
+    run.Capture("%s && pyenv which flake8", neovim3),
+    path.FromHome("bin/flake8"),
+  )
 
   log.Info("Installing pip dependencies")
   run.Verbose("pip2 install wakatime")
