78eabf198b6f5fbc2c9ff824a6409ac2c28a443d
Darren Cheng <darren@thanx.com>
2019-01-08 14:09:22 -0800

    Bump local mysql version.

--- commands/install/homebrew.go
+++ commands/install/homebrew.go
@@ -11,7 +11,7 @@ import (
 func (i Install) Homebrew() {
   log.Action("Installing Homebrew dependencies")
   run.Verbose("brew bundle --file=%s", path.FromDots("lib/Brewfile"))
-  run.Verbose("brew services start mysql@5.6")
+  run.Verbose("brew services start mysql@5.7")
   run.Verbose("brew services start postgresql")
   log.Info("Ensuring ~/.z exists")
   os.OpenFile(path.FromHome("Desktop/z"), os.O_RDONLY|os.O_CREATE, 0666)
