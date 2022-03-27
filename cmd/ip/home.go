af400fb66813cdece91d9f60264bc2407456ef9d
Darren Cheng <darren@thanx.com>
2022-03-27 11:10:49 -0700

    Cache `ip --home`.
    Resolves https://github.com/drn/dots/issues/103

--- cmd/ip/home.go
+++ cmd/ip/home.go
@@ -4,10 +4,14 @@ import (
 	"os"
 
 	"github.com/drn/dots/pkg/log"
+	"github.com/drn/dots/pkg/path"
 	"github.com/drn/dots/pkg/run"
 )
 
 func home() {
+	cachePath := path.FromHome(".dots/ip-home")
+	checkCache(cachePath)
 	ip := run.Capture("dig +short %s", os.Getenv("HOME_WAN"))
+	cache(cachePath, ip)
 	log.Info(ip)
 }
