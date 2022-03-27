20897aa003293b07a8facf0d3609e73f2d31bf78
Darren Cheng <darren@thanx.com>
2022-03-27 12:06:27 -0700

    Iterate on weather command formatting.

--- cmd/weather/root.go
+++ cmd/weather/root.go
@@ -1,11 +1,57 @@
 package main
 
 import (
+	"os"
+	"strings"
+
 	"github.com/drn/dots/pkg/log"
 	"github.com/drn/dots/pkg/run"
 )
 
 func main() {
-	condition := run.Capture("curl -s wttr.in?format=%%t+%%c")
-	log.Info(condition)
+	weather := run.Capture("curl -s wttr.in?format=%%t+%%x")
+	parts := strings.Split(weather, " ")
+	if len(parts) < 2 {
+		os.Exit(1)
+	}
+	temp := parts[0]
+	condition := parts[1]
+
+	log.Info("%c %s", conditionSymbol(condition), formatTemp(temp))
+}
+
+func formatTemp(temp string) string {
+	return temp[1 : len(temp)-1]
+}
+
+// For details, see:
+// - https://github.com/chubin/wttr.in/blob/master/lib/constants.py
+// - https://www.nerdfonts.com/cheat-sheet (mdi-weather)
+func conditionSymbol(condition string) rune {
+	switch condition {
+	case "mmm", "mm": // VeryCloudy, Cloudy
+		return '\ufa8f'
+	case "=": // Fog
+		return '\ufa90'
+	case "///", "//": // HeavyRain, HeavyShowers
+		return '\ufa95'
+	case "**", "*/*", "*": // HeavySnow, HeavySnowShowers, LightSnow
+		return '\ufa97'
+	case "/", ".": // LightRain, LightShowers
+		return '\ufa96'
+	case "x", "x/": // LightSleet, LightSleetShowers
+		return '\ufa91'
+	case "*/": // LightSnowShowers
+		return '\ufb7d'
+	case "m": // PartlyCloudy
+		return '\ufa94'
+	case "o": // Sunny
+		return '\ufa98'
+	case "/!/", "!/": // ThunderyHeavyRain, ThunderyShowers
+		return '\ufb7c'
+	case "*!*": // ThunderySnowShowers
+		return '\ufa92'
+	}
+	// Unknown
+	return '\ue348'
 }
