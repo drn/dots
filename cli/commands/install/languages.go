677039bbb39ef920b39bf1344fbc9024e9aa53fb
Darren Cheng <darren@thanx.com>
2022-12-20 20:43:45 -0800

    Update installed `golang` version.

--- cli/commands/install/languages.go
+++ cli/commands/install/languages.go
@@ -14,7 +14,7 @@ var versions = map[string]string{
 	"python3":   "3.9.1",
 	"terraform": "1.0.11",
 	"nodejs":    "17.2.0",
-	"golang":    "1.17.5",
+	"golang":    "1.19.2",
 }
 
 // Languages - Installs asdf & languages
