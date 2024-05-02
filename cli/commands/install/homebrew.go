3c63fc8b9cebfd07fae7f650601da245f8bd7f2f
Darren Cheng <darren@thanx.com>
2024-05-02 15:01:58 -0700

    Update DB version references.

--- cli/commands/install/homebrew.go
+++ cli/commands/install/homebrew.go
@@ -12,8 +12,8 @@ func (i Install) Homebrew() {
 	log.Action("Installing Homebrew dependencies")
 	exec("brew update")
 	exec("brew bundle --file=%s", path.FromDots("Brewfile"))
-	exec("brew services start mysql@5.7")
-	exec("brew services start postgresql")
+	exec("brew services start mysql@8.0")
+	exec("brew services start postgresql@16")
 	log.Info("Ensuring ~/.z exists")
 	os.OpenFile(path.FromHome("Desktop/z"), os.O_RDONLY|os.O_CREATE, 0666)
 }
